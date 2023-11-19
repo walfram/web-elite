664f33ab30839c41889e1347adfde35f0d16d7bb
walfram <jevgeni.kalinin@gmail.com>
2023-11-19 09:55:09 +0200

    minor planet list ui refactoring

--- apps/elite-galaxies/src/components/Planets.tsx
+++ apps/elite-galaxies/src/components/Planets.tsx
@@ -3,10 +3,9 @@ import {Button, Table} from "reactstrap";
 import {Seed} from "../galaxy/seed";
 import {useContext} from "react";
 import {GalaxySeedContext} from "../context/GalaxySeedContext";
-import {RiGovernmentLine, RiInformationLine} from "react-icons/ri";
-import {LiaIndustrySolid} from "react-icons/lia";
-import {GrTransaction} from "react-icons/gr";
+import {RiInformationLine} from "react-icons/ri";
 import {TbUfo} from "react-icons/tb";
+import {GiPayMoney} from "react-icons/gi";
 
 export default function Planets() {
 
@@ -17,6 +16,10 @@ export default function Planets() {
   const planets = galaxy(seed);
 
   const onPlanetInfo = (planet : Planet) => console.log('click on government', planet);
+
+  const onPlanetMarket = (planet: Planet) => console.log('show planet market', planet);
+
+  const onPlanetEquipment = (planet: Planet) => console.log('show planet equipment', planet);
 
   const bodyItems = planets.map((planet, idx) => {
     return (
@@ -32,9 +35,11 @@ export default function Planets() {
         <td>{planet.productivity}</td>
         <td>{speciesOf(planet.species)}</td>
         <td>
+          <div className="d-flex justify-content-between">
           <Button size="sm" onClick={() => onPlanetInfo(planet)}><RiInformationLine /></Button>
-          <Button size="sm" onClick={() => onPlanetMarket(planet)}><GrTransaction /></Button>
+          <Button size="sm" onClick={() => onPlanetMarket(planet)}><GiPayMoney /></Button>
           <Button size="sm" onClick={() => onPlanetEquipment(planet)}><TbUfo /></Button>
+          </div>
         </td>
       </tr>
     );
